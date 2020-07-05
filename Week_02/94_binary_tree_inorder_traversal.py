fbf74c1470c66554b261c37a9661d7f6aa873e91
Yi <yi@Yis-MacBook-Pro.lan>
2020-07-05 23:42:54 +0800

    solving 94_binary_tree_inorder_traversal

---
+++ Week_02/94_binary_tree_inorder_traversal.py
@@ -0,0 +1,15 @@
+class Solution(object):
+	def inorderTraversal(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[int]
+		"""
+		res = []
+		def dfs(root):
+			if not root:
+				return
+			dfs(root.left)
+			res.append(root.val)
+			dfs(root.right)
+		dfs(root)
+		return res
